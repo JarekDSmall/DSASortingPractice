8e1ccf3eb78f132279452728b7d4338a5fb3b468
JarekDSmall <129123790+JarekDSmall@users.noreply.github.com>
2023-10-06 20:25:10 -0400

    Add the Insertion Sort algorithm for efficient small list sorting
    input the correct file

--- insertion.js
+++ insertion.js
@@ -1,36 +1,14 @@
-function getDigit(num, i) {
-    return Math.floor(Math.abs(num) / Math.pow(10, i)) % 10;
-}
-
-function digitCount(num) {
-    if (num === 0) return 1;
-    return Math.floor(Math.log10(Math.abs(num))) + 1;
-}
-
-function mostDigits(nums) {
-    let maxDigits = 0;
-    for (let i = 0; i < nums.length; i++) {
-        maxDigits = Math.max(maxDigits, digitCount(nums[i]));
-    }
-    return maxDigits;
-}
-
-function radixSort(nums) {
-    let maxDigitCount = mostDigits(nums);
-    for (let k = 0; k < maxDigitCount; k++) {
-        let digitBuckets = Array.from({ length: 10 }, () => []);
-        for (let i = 0; i < nums.length; i++) {
-            let digit = getDigit(nums[i], k);
-            digitBuckets[digit].push(nums[i]);
+function insertionSort(arr) {
+    for (let i = 1; i < arr.length; i++) {
+        let currentVal = arr[i];
+        let j;
+        for (j = i - 1; j >= 0 && arr[j] > currentVal; j--) {
+            arr[j + 1] = arr[j];
         }
-        nums = [].concat(...digitBuckets);
+        arr[j + 1] = currentVal;
     }
-    return nums;
+    return arr;  // Ensure the sorted array is returned
 }
 
-module.exports = {
-    radixSort,
-    getDigit,
-    digitCount,
-    mostDigits
-};
+
+module.exports = insertionSort;
